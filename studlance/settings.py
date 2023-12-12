5114ec8dfeed99c7bc24c1d439fa1964f9b789b4
Topando <erdekov.sascha@yandex.ru>
2023-12-12 21:18:03 +0300

    `1`

--- studlance/settings.py
+++ studlance/settings.py
@@ -126,3 +126,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = []
