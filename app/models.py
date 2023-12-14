f07e208d9a2cbcb60b6b8454952c9cbb077d21d5
Topando <erdekov.sascha@yandex.ru>
2023-12-15 02:53:18 +0300

    Я машина1

--- app/models.py
+++ app/models.py
@@ -50,13 +50,22 @@ class Resume(models.Model):
 class University(models.Model):
     university = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.university
+
 
 class Course(models.Model):
     course = models.CharField(max_length=2)
 
+    def __str__(self):
+        return self.course
+
 
 class Direction(models.Model):
     direction = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.direction
 
 
 class Comments(models.Model):
@@ -66,7 +75,7 @@ class Comments(models.Model):
 
 
 class Task(models.Model):
-    customer_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='task_customer')
+    customer_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='task_customer', null=True, blank=True)
     executor_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     is_done = models.BooleanField(default=False)
     is_working = models.BooleanField(default=False)
--- app/urls.py
+++ app/urls.py
@@ -9,7 +9,10 @@ urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),
     path('tasks/', AllTasks.as_view(), name='tasks'),
-    path('task/<int:task_id>', AllTasks.as_view(), name='task'),
+    path('tasks/<int:task_id>/', TaskDetail.as_view(), name='task'),
+    path('tasks/create/', TaskCreate.as_view(), name='task_create'),
+    path('tasks/<int:task_id>/update/', TaskUpdate.as_view(), name='task_update'),
+    path('tasks/<int:task_id>/delete/', TaskDelete.as_view(), name='task_delete'),
     path("profile/<int:profile_id>/", Profile.as_view(), name="profile"),
     path('profile_update/', views.profile_update, name='profile_update'),
 ]
