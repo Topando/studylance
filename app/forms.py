3a052d6b3c658b41957aca1e2055a3df6552be7f
feorgng <feorgang@gmail.com>
2024-01-28 16:33:51 +0300

    rename

--- app/forms.py
+++ app/forms.py
@@ -11,9 +11,9 @@ class RegisterUserForm(UserCreationForm):
     first_name = forms.CharField(label="Имя")
     last_name = forms.CharField(label="Фамилия")
     username = forms.CharField(label="Логин", widget=forms.TextInput(attrs={"class": "form-input"}))
-    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={"class": "form-input"}))
+    email = forms.EmailField(label="Почта", widget=forms.EmailInput(attrs={"class": "form-input"}))
     password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={"class": "form-input"}))
-    password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput(attrs={"class": "form-input"}))
+    password2 = forms.CharField(label="Повторите пароль", widget=forms.PasswordInput(attrs={"class": "form-input"}))
 
     class Meta:
         model = User
